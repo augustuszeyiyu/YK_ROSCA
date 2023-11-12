dff653affe6df8aecc3619a90e8c60aebb343ffb
AugustusYu <augustuszeyiyu@gmail.com>
2023-11-13 01:56:00 +0800

    register page style make up

--- index/module/register/module.ts
+++ index/module/register/module.ts
@@ -42,29 +42,6 @@ import { User } from "../../../api/data-type/register";
 			view.element.innerHTML = layout.innerHTML;
 			view.relink();
 			layout.remove();
-
-			// Develope Help rigion
-			const accessor = view.input_data;
-
-			accessor.nid.value = 'F123456777';
-			accessor.name.value = '崊老師';
-			accessor.password.value = 'A1234567';
-			accessor.gender.value = 'M';
-			accessor.birth_date.value = '1984-01-17';
-			accessor.address.value = '新北市汐止區連興街116號';
-			accessor.line_id.value = '';
-			accessor.contact_home_number.value="0226481872";
-			accessor.contact_mobile_number.value="0921136362";
-			accessor.bank_code.value;
-			accessor.branch_code.value;
-			accessor.bank_account_number.value;
-			accessor.emergency_nid.value="A1234567890";
-			accessor.emergency_contact.value="Cheny";
-			accessor.emergency_contact_number.value="0226481872";
-			accessor.emergency_contact_relation.value;
-			// accessor.referrer_nid.value;
-			// accessor.volunteer_nid.value;
-			accessor.relink();
 		}
 	});
 
@@ -76,23 +53,58 @@ import { User } from "../../../api/data-type/register";
 		// accessor.member_content.addClass="active"
 		const accessor = view.input_data;
 		console.log(accessor.member_content);
-		accessor.member_content.style.display="flex"
-		accessor.recommendation_info.style.display="none"
-		accessor.bank_info_content.style.display="none"
+		accessor.member_content.style.display="flex";
+		accessor.recommendation_info.style.display="none";
+		accessor.bank_info_content.style.display="none";
+
+		accessor.btn_open_tab_member.classList.add('active');
+		accessor.btn_open_tab_recommand.classList.remove('active');
+		accessor.btn_open_tab_bankinfo.classList.remove('active');
 	})
 	.on('open_tab_recommand',(e:any)=>{
 		const accessor = view.input_data;
 		console.log(accessor.recommendation_info);
-		accessor.member_content.style.display="none"
-		accessor.recommendation_info.style.display="flex"
-		accessor.bank_info_content.style.display="none"
+		accessor.member_content.style.display="none";
+		accessor.recommendation_info.style.display="flex";
+		accessor.bank_info_content.style.display="none";
+
+		accessor.btn_open_tab_member.classList.remove('active');
+		accessor.btn_open_tab_recommand.classList.add('active');
+		accessor.btn_open_tab_bankinfo.classList.remove('active');
 	})
 	.on('open_tab_bankinfo',(e:any)=>{
 		const accessor = view.input_data;
 		console.log(accessor.bank_info_content);
-		accessor.member_content.style.display="none"
-		accessor.recommendation_info.style.display="none"
-		accessor.bank_info_content.style.display="flex"
+		accessor.member_content.style.display="none";
+		accessor.recommendation_info.style.display="none";
+		accessor.bank_info_content.style.display="flex";
+
+		accessor.btn_open_tab_member.classList.remove('active');
+		accessor.btn_open_tab_recommand.classList.remove('active');
+		accessor.btn_open_tab_bankinfo.classList.add('active');
+	})     
+	.on('load_test_register_userinfo',(e:any)=>{
+		// Develope Help rigion
+		const accessor = view.input_data;
+		accessor.nid.value = 'F123456777';
+		accessor.name.value = '崊老師';
+		accessor.password.value = 'A1234567';
+		accessor.gender.value = 'M';
+		accessor.birth_date.value = '1984-01-17';
+		accessor.address.value = '新北市汐止區連興街116號';
+		accessor.line_id.value = '';
+		accessor.contact_home_number.value = "0226481872";
+		accessor.contact_mobile_number.value = "0921136362";
+		accessor.bank_code.value;
+		accessor.branch_code.value;
+		accessor.bank_account_number.value;
+		accessor.emergency_nid.value = "A1234567890";
+		accessor.emergency_contact.value = "Cheny";
+		accessor.emergency_contact_number.value = "0226481872";
+		accessor.emergency_contact_relation.value;
+		// accessor.referrer_nid.value;
+		// accessor.volunteer_nid.value;
+		accessor.relink();
 	});
 
 	async function do_register_user(){
